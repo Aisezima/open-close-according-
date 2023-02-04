48951ea2c11d580159a84c839a92459420573fb6
Aisezima <djakenova2004@gmail.com>
2023-02-04 15:19:26 +0600

    according

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Chavo from './components/chavo/Chavo';
+
+function App() {
+  return (
+    <div className="App">
+      <Chavo/>
+    </div>
+  );
+}
+
+export default App;
